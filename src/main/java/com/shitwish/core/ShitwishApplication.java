060c272f64321fb8addee2f20b169fc19f59d419
Tovics <tovics1@gmail.com>
2018-08-27 13:25:29 +0200

    initial commit

---
+++ src/main/java/com/shitwish/core/ShitwishApplication.java
@@ -0,0 +1,12 @@
+package com.shitwish.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShitwishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShitwishApplication.class, args);
+    }
+}
---
+++ src/test/java/com/shitwish/core/ShitwishApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shitwish.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShitwishApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
