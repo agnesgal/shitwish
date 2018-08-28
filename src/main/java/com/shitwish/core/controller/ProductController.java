a8a684a62c2ddca62e91508b1e2400b5ea53afe9
Andras Zibar <zibandris@gmail.com>
2018-08-28 13:28:41 +0200

    skeleton begin

--- src/main/java/com/shitwish/core/controller/ProductController.java
+++ src/main/java/com/shitwish/core/controller/ProductController.java
@@ -1,4 +1,23 @@
 package com.shitwish.core.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ProductController {
+
+    @GetMapping("/")
+    public String displayProducts(Model model){
+
+        return "index";
+    }
+
+    @GetMapping("/product")
+    public String displayOneProduct(Model model, @RequestParam("id") String id){
+
+        return "product_view";
+    }
+
 }
