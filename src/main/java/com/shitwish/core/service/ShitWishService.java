5e73aa67777681ef73b2376e44ec1c42317d8d66
Dóra Surmann <surmann.dora@gmail.com>
2018-08-28 10:54:03 +0200

    basic templates service class created

---
+++ src/main/java/com/shitwish/core/service/ShitWishService.java
@@ -0,0 +1,15 @@
+package com.shitwish.core.service;
+
+
+import org.json.JSONException;
+import org.springframework.stereotype.Service;
+import org.json.JSONObject;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+
+@Service
+public class ShitWishService {
+
+}
