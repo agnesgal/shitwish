2ebef47f6486caf7c26c364a6e2e9b8a4e17e796
Ágnes Gál <galagnes277@gmail.com>
2018-08-28 15:24:02 +0200

    basic cart model

--- src/main/java/com/shitwish/core/model/Cart.java
+++ src/main/java/com/shitwish/core/model/Cart.java
@@ -1,4 +1,23 @@
 package com.shitwish.core.model;
 
+import java.util.ArrayList;
+
 public class Cart {
+
+    private User user;
+    private ArrayList<Integer> productList;
+
+
+    public ArrayList<Integer> getProductList() {
+        return productList;
+    }
+
+    public void addProduct(int productId) {
+        productList.add(productId);
+    }
+
+    public Cart(User user, ArrayList<Integer> productList) {
+        this.user = user;
+        this.productList = productList;
+    }
 }
--- src/main/java/com/shitwish/core/service/CartService.java
+++ src/main/java/com/shitwish/core/service/CartService.java
@@ -1,4 +1,10 @@
 package com.shitwish.core.service;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CartService {
+
+
+
 }
