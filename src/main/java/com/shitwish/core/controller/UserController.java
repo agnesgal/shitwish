ee844799735c5d84c9c83cedea8a9368fc1320ef
Tovics <tovics1@gmail.com>
2018-08-28 13:58:24 +0200

    user profile.html skeleton ready, need to fill up with data from backend

--- src/main/java/com/shitwish/core/controller/UserController.java
+++ src/main/java/com/shitwish/core/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.shitwish.core.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/user/{id}/profile")
+    public String getUserProfile() {
+        return "user_profile";
+    }
+
 }
