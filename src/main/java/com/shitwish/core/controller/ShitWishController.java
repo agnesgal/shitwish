4c99ac0f8fcb4dbeaec3324c4043fed6d7221ef7
Dóra Surmann <surmann.dora@gmail.com>
2018-08-28 12:06:36 +0200

    templates skeleton

--- src/main/java/com/shitwish/core/controller/ShitWishController.java
+++ src/main/java/com/shitwish/core/controller/ShitWishController.java
@@ -1,10 +1,20 @@
 package com.shitwish.core.controller;
 
+import com.shitwish.core.service.ShitWishService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
 
 @Controller
 public class ShitWishController {
+
+    @Autowired
+    private ShitWishService shitWishService;
+
+    private String USERAPIURL = "https://microservices-userapp.herokuapp.com/user";
 
     @GetMapping("/")
     public String loadIndex(){
