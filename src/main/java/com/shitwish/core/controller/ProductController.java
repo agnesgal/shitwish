7a93f429d116cb08c85198a25fb877b3a01750dd
Andras Zibar <zibandris@gmail.com>
2018-08-29 11:27:50 +0200

    product controller skeleton

--- src/main/java/com/shitwish/core/controller/ProductController.java
+++ src/main/java/com/shitwish/core/controller/ProductController.java
@@ -1,8 +1,10 @@
 package com.shitwish.core.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -11,13 +13,31 @@ public class ProductController {
     @GetMapping("/")
     public String displayProducts(Model model){
 
+        //TODO request to heroku product api, add result to model
+        //List<Product> products = ???
+
         return "index";
     }
 
     @GetMapping("/product")
     public String displayOneProduct(Model model, @RequestParam("id") String id){
 
+        //TODO request to heroku product api, add result to model
+        //Product product = ???
+
         return "product_view";
     }
 
+    @GetMapping("/add")
+    public String showNewProductForm(){
+
+        return "product_form";
+    }
+
+    @PostMapping("/add")
+    public String addNewProduct(){
+
+
+        return "redirect:/";
+    }
 }
