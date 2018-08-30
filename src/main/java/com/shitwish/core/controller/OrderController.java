70eee69b9776d3bbefcd90431b56c2805f9a51bb
martaJesztak <jesztakm@gmail.com>
2018-08-30 13:54:41 +0200

    Modal added for Order history.

--- src/main/java/com/shitwish/core/controller/OrderController.java
+++ src/main/java/com/shitwish/core/controller/OrderController.java
@@ -22,6 +22,6 @@ public class OrderController {
         model.addAttribute("user");
         Order[] orders = orderService.getOrderHistory(1);
         model.addAttribute("order_history", orders);
-        return "order";
+        return "index";
     }
 }
